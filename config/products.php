2a16b5bd0cc393cdf474c3f569761a3d68ec401a
Ashley Grant <agrant2@nic.edu>
2024-03-26 21:10:06 -0700

    Following Brian's video to help

--- config/products.php
+++ config/products.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("db:3306", "root", "root", "products");
+$conn = mysqli_connect("db", "root", "root", "products");
 
 if(mysqli_connect_errno()) {
     echo "Database error: " . mysqli_connect_error();
--- index.php
+++ index.php
@@ -3,10 +3,11 @@
     require('./config/products.php'); 
 
     $sql = "SELECT id,name,price FROM items";
+    //* means selects all so technically don't need id,name,price
 
     // query the DB and add the result to a php array
     $result = mysqli_query($conn, $sql);
-    $items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
     
     // free resultfrom memory and close SQL connection
     mysqli_free_result($result);
@@ -21,34 +22,44 @@
 <head>  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css">
     <title>Products</title>
+    <link rel="stylesheet" href="./assets/style.css">
 </head>
 <body>
 
-<main>
-    <h1>OUR PRODUCTS</h1>
+    <main>
+        <h1>OUR PRODUCTS</h1>
 
-    <table>
+        <table>
 
-        <tr class="columns">
-            <th>Name</th>
-            <th>Price</th>
-        </tr>
+            <thead>
+                <tr class="columns">
+                    <td>Name</td>
+                    <td>Price</td>
+                </tr>
 
-        <tr>  
-            <?php foreach ($items as $item) :  ?>
-        <th>
-            <?php echo $item["name"]; ?>  
-        <th>
-            <?php echo $item["price"]; ?>      
-        </th>
+                <!-- <tr class="columns">
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr> -->
+            </thead>
 
-        </tr>
+            <tbody>
 
-        <?php endforeach ?>
-    </table>
-</main>
+                <?php foreach ($products as $product) : ?>
+
+                    <tr>
+                        <td><?php echo $product["name"]; ?></td>
+                        <td><?php echo "$" . $product["price"]; ?></td>
+                    </tr>
+
+            </tbody>
+
+            <?php endforeach ?>
+
+        </table>
+
+    </main>
 
 </body>
 </html>

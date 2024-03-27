bf0d59867f1b9fd2207912e3cba083c1e8013a5b
Ashley Grant <agrant2@nic.edu>
2024-03-26 21:43:17 -0700

    fixed styling

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
-    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/style2.css">
 </head>
 <body>
 

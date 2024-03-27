04936fd82bd8a2bf36f19b328016fa987e4f386f
Ashley Grant <agrant2@nic.edu>
2024-03-26 22:15:27 -0700

    style update

--- config/products.php
+++ config/products.php
@@ -1,6 +1,8 @@
 <?php
 
-$conn = mysqli_connect("db", "root", "root", "products");
+// $conn = mysqli_connect("db", "root", "root", "products"); // Local
+$conn = mysqli_connect("127.0.0.1:3308", "2024agrant", "agrant494", "welearnd_gdes261_2024_agrant"); // Remote
+
 
 if(mysqli_connect_errno()) {
     echo "Database error: " . mysqli_connect_error();
